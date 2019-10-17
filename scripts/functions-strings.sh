2ae29519449d1069f7d935a5a30288f51f7a6c5c
wildone <max.barrass@gmail.com>
2019-10-17 23:50:50 +1100

    return false if input is empty

--- scripts/functions-strings.sh
+++ scripts/functions-strings.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 function isStringMatchesRegex() {
-    local STRING=${1?Need string}
-    local REGEX=${2?Need regex}
-   echo "${STRING}" | $GREP -Eq "$REGEX" && echo "true" || echo "false"
+  local STRING=${1?Need string}
+  local REGEX=${2?Need regex}
+  if [[ -z $STRING ]]; then
+    echo "false"
+  fi
+  echo "${STRING}" | $GREP -Eq "$REGEX" && echo "true" || echo "false"
 }
