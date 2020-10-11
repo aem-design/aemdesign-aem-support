a8ba98e6be850b54dc5089137677000242ecbaea
Chris Shaw <me@cshaw.xyz>
2020-10-12 00:03:50 +1100

    Added TypeScript path mappings for compose
    Path mappings allow Jest to automatically resolve TypeScript paths without the need of manually resolving them.

--- aemdesign-aem-compose/jest.config.js
+++ aemdesign-aem-compose/jest.config.js
@@ -1,9 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+const tsConfig = require('./tsconfig.json')
+
 /** @typedef {import('ts-jest')} */
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
   displayName: {
     color : 'blue',
     name  : '@aem-design/compose',
+  },
+
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(tsConfig.compilerOptions.paths, { prefix : '<rootDir>/' }),
   },
 }
 
