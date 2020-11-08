01c91702b9882238af4253258d88ea34741a9b52
Chris Shaw <me@cshaw.xyz>
2020-11-08 23:53:10 +1100

    Cleaned up vendor.ts [skip-ci]

--- aemdesign-aem-compose/src/core/js/vendor.ts
+++ aemdesign-aem-compose/src/core/js/vendor.ts
@@ -1,11 +1,3 @@
 import $ from 'jquery'
 
-function loadVendor() {
-  console.log('vendor.js jQuery Version', $.fn.jquery)
-}
-
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', loadVendor)
-} else {
-  loadVendor()
-}
+console.log('vendor.js jQuery Version', $.fn.jquery)
