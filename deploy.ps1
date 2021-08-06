ff14ace2b46bc7da27112d70b9cb8add2ee02976
wildone <max.barrass@gmail.com>
2021-08-06 15:41:40 +1000

    add testing helpers.

--- deploy.ps1
+++ deploy.ps1
@@ -1,22 +1,33 @@
 Param(
-  [string]$LOG_PATH = "${PWD}\logs",
+  [string]$LOG_PATH = "logs",
   [string]$LOG_PEFIX = "deploy",
   [string]$LOG_SUFFIX = ".log",
-  [string]$DOCKER_LOGS_FOLDER = "${PWD}\logs\docker",
+  [string]$DOCKER_LOGS_FOLDER = "logs\docker",
   [string]$AEM_SCHEME = "http",
   [string]$AEM_HOST = "localhost",
   [string]$AEM_PORT = "4502",
   [string]$AEM_USERNAME = "admin",
   [string]$AEM_PASSWORD = "admin",
-  [string]$MVN_COMMAND = "mvn -D""vault.useProxy=false"" -DskipTests -e -U -P installdeploymentpackage clean install -D""aem.port=$AEM_PORT"" -D""aem.host=$AEM_HOST"" -D""aem.username=$AEM_USERNAME"" -D""aem.password=$AEM_PASSWORD"" -D""aem.scheme=$AEM_SCHEME"" "
+  [string]$MVN_COMMAND = "mvn -DskipTests -e -U -P autoInstallPackage clean install -D""aem.port=$AEM_PORT"" -D""aem.host=$AEM_HOST"" -D""aem.username=$AEM_USERNAME"" -D""aem.password=$AEM_PASSWORD"" -D""aem.scheme=$AEM_SCHEME"" ",
+  [string]$FUNCTIONS_URI = "https://github.com/aem-design/aemdesign-docker/releases/latest/download/functions.ps1",
+  # Disable services before import
+  [string[]]$SERVICES_TO_DISABLE = @(
+    "/system/console/bundles/com.day.cq.cq-mailer",
+    "/system/console/bundles/com.adobe.granite.workflow.core"
+  )
 
 )
 
-. ".\scripts\functions.ps1"
+$SKIP_CONFIG = $true
+$PARENT_PROJECT_PATH = "."
 
-printSectionBanner "Deploying Monolith Package:" "warn"
-printSectionLine "mvn -Dvault.useProxy=false -DskipTests -e -U -P installdeploymentpackage clean install"
+. ([Scriptblock]::Create((([System.Text.Encoding]::ASCII).getString((Invoke-WebRequest -Uri "${FUNCTIONS_URI}").Content))))
 
+$script:LOG_PATH = $LOG_PATH
+$script:TEST_SELENIUM_URL = $TEST_SELENIUM_URL
+
+printSectionBanner "Deploying Monolith Package"
+printSectionLine ("$MVN_COMMAND" -replace "$AEM_PASSWORD", "***")
 
 #update host
 if ( $TEST_HOST -eq "localhost" )
@@ -25,32 +36,34 @@ if ( $TEST_HOST -eq "localhost" )
   $script:TEST_HOST="${LOCAL_IP}"
 }
 
-$script:ADDRESS="${AEM_SCHEMA}://${AEM_HOST}:${AEM_PORT}"
-$script:AEM_AVAILABLE=$(testServer "${ADDRESS}")
+$script:AEM_AVAILABLE=$(testServer "${AEM_SCHEME}://${AEM_HOST}:${AEM_PORT}")
 
 printSectionLine "Is AEM at ${AEM_SCHEME}://${AEM_HOST}:${AEM_PORT} available? ${AEM_AVAILABLE}"
 
 if ( $AEM_AVAILABLE )
 {
+
+  if ($SERVICES_TO_DISABLE)
+  {
+    Write-Output "------- Disable Services ----------"
+    $SERVICES_TO_DISABLE | ForEach-Object {
+        Write-Output "Stopping service: $_"
+        doSlingPost -Method Post -Referer $ADDRESS -UserAgent "curl" -Body $BODY_SERVICE_TO_DISABLE -Url "${ADDRESS}$_" -BasicAuthCreds ${AEM_USER}:${AEM_PASSWORD} -Timeout $TIMEOUT
+    }
+  }
+
   printSectionLine "AEM host is available!" "info"
-
-  printSectionBanner "Disable Workflows" "warn"
-  doSlingPost -Method Post -Referer $ADDRESS -UserAgent "curl" -Body $WORKFLOW_ASSET_DISABLE_UPDATE -Url "${ADDRESS}${WORKFLOW_ASSET_MODIFY}" -BasicAuthCreds ${AEM_USERNAME}:${AEM_PASSWORD} -Timeout $TIMEOUT
-  doSlingPost -Method Post -Referer $ADDRESS -UserAgent "curl" -Body $WORKFLOW_ASSET_DISABLE_CREATE -Url "${ADDRESS}${WORKFLOW_ASSET_CREATE}" -BasicAuthCreds ${AEM_USERNAME}:${AEM_PASSWORD} -Timeout $TIMEOUT
-
-#   printSectionBanner "Disable aem mailer bundle" "warn"
-#   doSlingPost -Method Post -Referer $ADDRESS -UserAgent "curl" -Body $BODY_SERVICE_TO_DISABLE -Url "${ADDRESS}${SERVICE_TO_DISABLE}" -BasicAuthCreds ${AEM_USERNAME}:${AEM_PASSWORD} -Timeout $TIMEOUT
-
-  printSectionLine "Deploying!" "info"
+  printSectionLine "Deploying:" "info"
   Invoke-Expression -Command "$MVN_COMMAND" | Tee-Object -Append -FilePath "${LOG_FILE}"
 
-
-  printSectionBanner "Enable Workflows" "warn"
-  doSlingPost -Method Post -Referer $ADDRESS -UserAgent "curl" -Body $WORKFLOW_ASSET_ENABLE_UPDATE -Url "${ADDRESS}${WORKFLOW_ASSET_MODIFY}" -BasicAuthCreds ${AEM_USERNAME}:${AEM_PASSWORD} -Timeout $TIMEOUT
-  doSlingPost -Method Post -Referer $ADDRESS -UserAgent "curl" -Body $WORKFLOW_ASSET_ENABLE_CREATE -Url "${ADDRESS}${WORKFLOW_ASSET_CREATE}" -BasicAuthCreds ${AEM_USERNAME}:${AEM_PASSWORD} -Timeout $TIMEOUT
-
-#   printSectionBanner "Enable aem mailer bundle" "info"
-#   doSlingPost -Method Post -Referer $ADDRESS -UserAgent "curl" -Body $BODY_SERVICE_TO_DISABLE_ENABLE -Url "${ADDRESS}${SERVICE_TO_DISABLE}" -BasicAuthCreds ${AEM_USERNAME}:${AEM_PASSWORD} -Timeout $TIMEOUT
+  if ($SERVICES_TO_DISABLE)
+  {
+      Write-Output "------- Enable Services ----------"
+      $SERVICES_TO_DISABLE | ForEach-Object {
+          Write-Output "Stopping service: $_"
+          doSlingPost -Method Post -Referer $ADDRESS -UserAgent "curl" -Body $BODY_SERVICE_TO_DISABLE_ENABLE -Url "${ADDRESS}$_" -BasicAuthCreds ${AEM_USER}:${AEM_PASSWORD} -Timeout $TIMEOUT
+      }
+  }
 
 
 } else {
